039c9a952bd32dc0682993c35e49650d628a0806
JCMcGraw <49905256+JCMcGraw@users.noreply.github.com>
2019-11-14 11:30:09 +0100

    AnimalManager Example

---
+++ AnimalHousePersistence/AnimalManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace AnimalHousePersistence
+{
+    class AnimalManager
+    {
+        public void GetAnimal(int animalID)
+        {
+            string query = "";
+
+            SQLQuery sQLQuery = new SQLQuery(query);
+
+            sQLQuery.AddParameter("animalid", animalID.ToString(), SqlDbType.Int);
+
+            SQLQueryResult sQLQueryResult = SQLDatabaseConnector.QueryDatabase(sQLQuery);
+
+            if (sQLQueryResult.code == 0)
+            {
+                //read datatable
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
