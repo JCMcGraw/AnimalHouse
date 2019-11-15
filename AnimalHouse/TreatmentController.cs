7fa0986e731bffa0a533d97e80cbbae12e09bfd3
Daniel Raffnsøe Dall <dani8941@edu.eal.dk>
2019-11-15 13:03:18 +0100

    Treatment Use case

---
+++ AnimalHouse/TreatmentController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnimalHousePersistence;
+using AnimalHouseEntities;
+
+namespace AnimalHouse
+{
+    class TreatmentController
+    {
+        ITreatmentManager treatmentManager = new TreatmentManager();
+
+        public string CreateTreatment(Treatment treatment)
+        {
+            string createTreatment = treatmentManager.CreateTreatment(treatment);
+            return createTreatment;
+        }
+
+        public string UpdateTreatment(Treatment treatment)
+        {
+            string updateTreatment = treatmentManager.UpdateTreatment(treatment);
+            return updateTreatment;
+        }
+
+        public string DeleteTreatment(Treatment treatment)
+        {
+            string deleteTreatment = treatmentManager.DeleteTreatment(treatment);
+            return deleteTreatment;
+        }
+
+        public string GetTreatment(int treatmentID)
+        {
+            string getTreatment = treatmentManager.GetTreatment(treatmentID);
+            return getTreatment;
+        }
+
+        public string GetManyTreatmentsByEmployee(int employeeID, DateTime startTime, DateTime endTime)
+        {
+            string getManyTreatmentsByEmployee = treatmentManager.GetManyTreatmentsByEmployee(employeeID,startTime,endTime);
+            return getManyTreatmentsByEmployee;
+        }
+
+        public string GetManyTreatmentsByAnimal(int animalID, DateTime startTime, DateTime endTime)
+        {
+            string getManyTreatmentsByAnimal = treatmentManager.GetManyTreatmentsByAnimal(animalID,startTime,endTime);
+            return getManyTreatmentsByAnimal;
+        }
+    }
+}
--- AnimalHousePersistence/ITreatmentManager.cs
+++ AnimalHousePersistence/ITreatmentManager.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AnimalHouseEntities;
 
 namespace AnimalHousePersistence
 {
-    interface ITreatmentManager
+    public interface ITreatmentManager
     {
+        string CreateTreatment(Treatment treatment);
+
+        string UpdateTreatment(Treatment treatment);
+
+        string DeleteTreatment(Treatment treatment);
+
+        string GetTreatment(int treatmentID);
+
+        string GetManyTreatmentsByEmployee(int employeeID, DateTime startTime, DateTime endTime);
+
+        string GetManyTreatmentsByAnimal(int animalID, DateTime startTime, DateTime endTime);
     }
 }
