d5677db00868b08094911141a3a5b446b45aefad
Daniel Raffnsøe Dall <dani8941@edu.eal.dk>
2019-11-18 15:02:08 +0100

    sdf

--- AnimalHousePersistence/TreatmentManager.cs
+++ AnimalHousePersistence/TreatmentManager.cs
@@ -70,16 +70,16 @@ namespace AnimalHousePersistence
             SQLQueryResult sQLQueryResult = SQLDatabaseConnector.QueryDatabase(sQLQuery);
 
             int tratmentID = (int)sQLQueryResult.dataTable.Rows[0]["TreatmentID"];
+            int treatmentTypeID = (int)sQLQueryResult.dataTable.Rows[0]["TreatmentTypeID"];
+            int operationRoomID = (int)sQLQueryResult.dataTable.Rows[0]["OperationRoomID"];
+            int cageID = (int)sQLQueryResult.dataTable.Rows[0]["CageID"];
+            int itemID = (int)sQLQueryResult.dataTable.Rows[0]["ItemID"];
             DateTime startTime = (DateTime)sQLQueryResult.dataTable.Rows[0]["StartTime"];
             DateTime endTime = (DateTime)sQLQueryResult.dataTable.Rows[0]["EndTime"];
             bool payed = (bool)sQLQueryResult.dataTable.Rows[0]["Payed"];
-            int cageID = (int)sQLQueryResult.dataTable.Rows[0]["CageID"];
-            int treatmentTypeID = (int)sQLQueryResult.dataTable.Rows[0]["TreatmentTypeID"];
-            int operationRoomID = (int)sQLQueryResult.dataTable.Rows[0]["OperationRoomID"];
             int employeeID = (int)sQLQueryResult.dataTable.Rows[0]["EmployeeID"];
             int animalID = (int)sQLQueryResult.dataTable.Rows[0]["AnimalID"];
-            int itemID = (int)sQLQueryResult.dataTable.Rows[0]["ItemID"];
-
+            
             Treatment treatment = TreatmentFaktory.Instance().CreateTreatment(tratmentID, treatmentTypeID, operationRoomID,cageID,itemID,startTime,endTime,payed);
 
             return treatment;
@@ -102,15 +102,15 @@ namespace AnimalHousePersistence
             for (int i = 0; i < sQLQueryResult.dataTable.Rows.Count; i++)
             {
                 int tratmentID = (int)sQLQueryResult.dataTable.Rows[i]["TreatmentID"];
+                int treatmentTypeID = (int)sQLQueryResult.dataTable.Rows[i]["TreatmentTypeID"];
+                int operationRoomID = (int)sQLQueryResult.dataTable.Rows[i]["OperationRoomID"];
+                int cageID = (int)sQLQueryResult.dataTable.Rows[i]["CageID"];
+                int itemID = (int)sQLQueryResult.dataTable.Rows[i]["ItemID"];
                 DateTime startTimee = (DateTime)sQLQueryResult.dataTable.Rows[i]["StartTime"];
                 DateTime endTimee = (DateTime)sQLQueryResult.dataTable.Rows[i]["EndTime"];
                 bool payed = (bool)sQLQueryResult.dataTable.Rows[i]["Payed"];
-                int cageID = (int)sQLQueryResult.dataTable.Rows[i]["CageID"];
-                int treatmentTypeID = (int)sQLQueryResult.dataTable.Rows[i]["TreatmentTypeID"];
-                int operationRoomID = (int)sQLQueryResult.dataTable.Rows[i]["OperationRoomID"];
                 int employeeIDD = (int)sQLQueryResult.dataTable.Rows[i]["EmployeeID"];
                 int animalID = (int)sQLQueryResult.dataTable.Rows[i]["AnimalID"];
-                int itemID = (int)sQLQueryResult.dataTable.Rows[i]["ItemID"];
 
                 treatments.Add(TreatmentFaktory.Instance().CreateTreatment(tratmentID, treatmentTypeID, operationRoomID, cageID, itemID, startTimee, endTimee, payed));
             }
@@ -134,15 +134,15 @@ namespace AnimalHousePersistence
             for (int i = 0; i < sQLQueryResult.dataTable.Rows.Count; i++)
             {
                 int tratmentID = (int)sQLQueryResult.dataTable.Rows[i]["TreatmentID"];
+                int treatmentTypeID = (int)sQLQueryResult.dataTable.Rows[i]["TreatmentTypeID"];
+                int operationRoomID = (int)sQLQueryResult.dataTable.Rows[i]["OperationRoomID"];
+                int cageID = (int)sQLQueryResult.dataTable.Rows[i]["CageID"];
+                int itemID = (int)sQLQueryResult.dataTable.Rows[i]["ItemID"];
                 DateTime startTimee = (DateTime)sQLQueryResult.dataTable.Rows[i]["StartTime"];
                 DateTime endTimee = (DateTime)sQLQueryResult.dataTable.Rows[i]["EndTime"];
                 bool payed = (bool)sQLQueryResult.dataTable.Rows[i]["Payed"];
-                int cageID = (int)sQLQueryResult.dataTable.Rows[i]["CageID"];
-                int treatmentTypeID = (int)sQLQueryResult.dataTable.Rows[i]["TreatmentTypeID"];
-                int operationRoomID = (int)sQLQueryResult.dataTable.Rows[i]["OperationRoomID"];
-                int employeeIDD = (int)sQLQueryResult.dataTable.Rows[i]["EmployeeID"];
+                int employeeID = (int)sQLQueryResult.dataTable.Rows[i]["EmployeeID"];
                 int animalIDD = (int)sQLQueryResult.dataTable.Rows[i]["AnimalID"];
-                int itemID = (int)sQLQueryResult.dataTable.Rows[i]["ItemID"];
 
                 treatments.Add(TreatmentFaktory.Instance().CreateTreatment(tratmentID, treatmentTypeID, operationRoomID, cageID, itemID, startTimee, endTimee, payed));
             }
@@ -165,15 +165,15 @@ namespace AnimalHousePersistence
             for (int i = 0; i < sQLQueryResult.dataTable.Rows.Count; i++)
             {
                 int tratmentID = (int)sQLQueryResult.dataTable.Rows[i]["TreatmentID"];
+                int treatmentTypeID = (int)sQLQueryResult.dataTable.Rows[i]["TreatmentTypeID"];
+                int operationRoomID = (int)sQLQueryResult.dataTable.Rows[i]["OperationRoomID"];
+                int cageID = (int)sQLQueryResult.dataTable.Rows[i]["CageID"];
+                int itemID = (int)sQLQueryResult.dataTable.Rows[i]["ItemID"];
                 DateTime startTimee = (DateTime)sQLQueryResult.dataTable.Rows[i]["StartTime"];
                 DateTime endTimee = (DateTime)sQLQueryResult.dataTable.Rows[i]["EndTime"];
                 bool payed = (bool)sQLQueryResult.dataTable.Rows[i]["Payed"];
-                int cageID = (int)sQLQueryResult.dataTable.Rows[i]["CageID"];
-                int treatmentTypeID = (int)sQLQueryResult.dataTable.Rows[i]["TreatmentTypeID"];
-                int operationRoomID = (int)sQLQueryResult.dataTable.Rows[i]["OperationRoomID"];
-                int employeeIDD = (int)sQLQueryResult.dataTable.Rows[i]["EmployeeID"];
-                int animalIDD = (int)sQLQueryResult.dataTable.Rows[i]["AnimalID"];
-                int itemID = (int)sQLQueryResult.dataTable.Rows[i]["ItemID"];
+                int employeeID = (int)sQLQueryResult.dataTable.Rows[i]["EmployeeID"];
+                int animalID = (int)sQLQueryResult.dataTable.Rows[i]["AnimalID"];
 
                 treatments.Add(TreatmentFaktory.Instance().CreateTreatment(tratmentID, treatmentTypeID, operationRoomID, cageID, itemID, startTimee, endTimee, payed));
             }
