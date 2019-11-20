88e13060f509d3c3b347224a0939fc91cf0b3bf1
Daniel Raffnsøe Dall <dani8941@edu.eal.dk>
2019-11-20 15:01:41 +0100

    UC19 DONE

---
+++ AnimalHouseEntities/TreatmentTypeFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalHouseEntities
+{
+    public class TreatmentTypeFactory
+    {
+        private static TreatmentTypeFactory instance;
+
+        private TreatmentTypeFactory() { }
+
+        public static TreatmentTypeFactory Instance()
+        {
+            if (instance == null)
+            {
+                instance = new TreatmentTypeFactory();
+            }
+
+            return instance;
+        }
+
+        public TreatmentType CreateTreatmentType(int treatmentTypeID, string name)
+        {
+            return new TreatmentType(treatmentTypeID, name);
+        }
+    }
+}
