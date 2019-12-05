33545f2b732d9ad6b7bc0f1b07c64fe1de562e40
JCMcGraw <49905256+JCMcGraw@users.noreply.github.com>
2019-12-05 13:17:40 +0100

    safdsad

---
+++ AnimalHouseUnitTest/SaleUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnimalHouseUnitTest
+{
+    /// <summary>
+    /// Summary description for SaleUnitTest
+    /// </summary>
+    [TestClass]
+    public class SaleUnitTest
+    {
+        
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
