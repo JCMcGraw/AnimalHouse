2defdb3763824c9476397b5eeb3fc69c47bf820d
JCMcGraw <49905256+JCMcGraw@users.noreply.github.com>
2019-12-09 10:23:33 +0100

    asdf

--- AnimalHouseUI/Properties/Resources.Designer.cs
+++ AnimalHouseUI/Properties/Resources.Designer.cs
@@ -139,5 +139,15 @@ namespace AnimalHouseUI.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap question_mark_32px {
+            get {
+                object obj = ResourceManager.GetObject("question_mark_32px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
