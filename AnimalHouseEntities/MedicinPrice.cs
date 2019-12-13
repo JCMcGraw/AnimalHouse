cfc4543c6b25fe19f7b29425f2937f4274ee5aab
Simeon Øe-Sørensen <do.it.like@gmail.com>
2019-12-13 09:25:10 +0100

    gdgfdgfdg

---
+++ AnimalHouseEntities/MedicinPrice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalHouseEntities
+{
+    class MedicinPrice
+    {
+        public DateTime Date { get; private set; }
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+
+        public MedicinPrice(DateTime date, string name, decimal price)
+        {
+            this.Date = date;
+            this.Name = name;
+            this.Price = price;
+
+        }
+        public void UpdateMedicin(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
